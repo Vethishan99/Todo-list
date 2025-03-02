0a8ac1e979c73fb047f13327a35ca9f528e11aa8
Vethishan <vethishan96@gmail.com>
2025-03-02 12:47:56 +0530

    Minor bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,36 @@ import AddItem from "./AddItem";
 
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("todo_list") || "[]")
+    JSON.parse(localStorage.getItem("todo_list")) || []
   );
+
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const addNewItem = { id, checked: false, name: item };
     const listItems = [...items, addNewItem];
     setItems(listItems);
-    localStorage.setList("todo_list", JSON.stringify(listItems));
+    localStorage.setItem("todo_list", JSON.stringify(listItems));
   };
+
   const [newItem, setNewItem] = useState("");
+
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
-    localStorage.setList("todo_list", JSON.stringify(listItems));
+    localStorage.setItem("todo_list", JSON.stringify(listItems));
   };
+
   const handleDelete = (id) => {
     const deleteItems = items.filter((item) => item.id !== id);
     setItems(deleteItems);
-    localStorage.setList("todo_list", JSON.stringify(listItems));
+    localStorage.setItem("todo_list", JSON.stringify(deleteItems));
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!newItem) return;
-    console.log(newItem);
     setNewItem("");
     addItem(newItem);
   };
